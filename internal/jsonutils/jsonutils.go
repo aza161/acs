e71cbb5b3429d1b87083d122ff2e8484b2c10612
Ahmad <aza29@mail.aub.edu>
2025-11-01 01:38:04 +0200

    Implemented passowrd entry and encrypted blob models with encryption/decryption functions

---
+++ internal/jsonutils/jsonutils.go
@@ -0,0 +1,106 @@
+package jsonutils
+
+import (
+	encrypt "acs/pkg/crypt"
+	"acs/pkg/passutils"
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/json"
+	"runtime"
+	"time"
+)
+
+type EncryptedPasswords struct {
+	Salt          string `json:"salt"`
+	Time          uint32 `json:"time"`
+	Memory        uint32 `json:"memory"`
+	Threads       uint8  `json:"threads"`
+	Nonce         string `json:"gcm_nonce"`
+	EncryptedData string `json:"blob"`
+}
+
+// A passowrd entry model, exclusively used on client side
+type Entry struct {
+	URL        string    `json:"url"`
+	UserName   string    `json:"username"`
+	Password   string    `json:"password"`
+	CreateDate time.Time `json:"create_date"`
+	AccessDate time.Time `json:"access_date"`
+	Info       string    `json:"notes,omitempty"`
+}
+
+// A function that generates indented JSON stings
+func GenerateJson(object any) ([]byte, error) {
+	return json.MarshalIndent(object, "", "  ")
+}
+
+// A function that takes a password, generates an encryption key using Argon2ID,
+// converts the entries list to a JSON and then encrypts it using AES-GCM.
+// Returns EncryptPasswords type which contains the encrypted entries in Base64
+// and the other necessary parameters used for AES-GCM and Argon2ID.
+func EncryptPasswords(password string, entries []Entry) (EncryptedPasswords, error) {
+	// Gneerate random salt
+	salt := make([]byte, passutils.SaltLen)
+	rand.Read(salt)
+
+	// Generate encryption key using Argon2ID
+	threads := runtime.NumCPU()
+	key, err := passutils.Argon2ID(password, salt, uint8(threads))
+	if err != nil {
+		return EncryptedPasswords{}, err
+	}
+
+	// Serialized password entries
+	byteObj, err := GenerateJson(entries)
+
+	nonce, _ := encrypt.GenerateNonce(12)
+
+	// Encrypted password list
+	encryptedData, err := encrypt.EncryptAESGCM(key, nonce, byteObj, []byte("Encrypted_Passwords"))
+	if err != nil {
+		return EncryptedPasswords{}, err
+	}
+
+	// Converetd to Base64 because JSON supports stings only
+	jsonData := EncryptedPasswords{
+		EncryptedData: base64.StdEncoding.EncodeToString(encryptedData),
+		Salt:          base64.StdEncoding.EncodeToString(salt),
+		Nonce:         base64.StdEncoding.EncodeToString(nonce),
+		Memory:        passutils.Memory,
+		Time:          passutils.Time,
+		Threads:       uint8(threads),
+	}
+	return jsonData, nil
+}
+
+func DecryptPasswords(password string, passowrds EncryptedPasswords) ([]Entry, error) {
+	// Derrive Encryption key from the passowrd
+	salt, _ := base64.StdEncoding.DecodeString(passowrds.Salt)
+	key, err := passutils.Argon2ID(password, salt, passowrds.Threads)
+	if err != nil {
+		return nil, err
+	}
+
+	// Decode the nonce
+	nonce, err := base64.StdEncoding.DecodeString(passowrds.Nonce)
+	if err != nil {
+		return nil, err
+	}
+
+	// Decode the passwords
+	encryptedPasswords, err := base64.StdEncoding.DecodeString(passowrds.EncryptedData)
+	if err != nil {
+		return nil, err
+	}
+
+	// Decrypt the data
+	decryptedData, err := encrypt.DecryptAESGCM(key, nonce, encryptedPasswords, []byte("Encrypted_Passwords"))
+	if err != nil {
+		return nil, err
+	}
+
+	// De-Serialzie the data
+	var entries []Entry
+	err = json.Unmarshal(decryptedData, &entries)
+	return entries, err
+}
